af1aeeb35ff00c9330ccafd08f1abed945c18ea6
Joshua Kemp <josh.kemp@ziplist.com>
2014-11-29 11:49:41 -0500

    Adding todo controlller method actions and routes

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,13 @@
 class TodosController < ApplicationController
   def index
-    @todo_array = ['Buy Milk', 'Buy Soap', 'Pay bill', 'Draw Money']
+    # @todo_array = ['Buy Milk', 'Buy Soap', 'Pay bill', 'Draw Money']
+    @todo_items = Todo.all
   end
+
+  def delete
+    #put delete logic here
+    @todo_items = Todo.last
+    @todo_items.destroy
+  end
+
 end
