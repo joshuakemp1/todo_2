9793fb0a043ab2f3833a5ca01511b489f09b23fe
Joshua Kemp <josh.kemp@ziplist.com>
2014-12-03 15:09:13 -0500

    Adding a todos model test

--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -1,9 +1,25 @@
 require 'test_helper'
 
 class TodosControllerTest < ActionController::TestCase
+  setup do
+    @todo_items = todos(:one)
+  end
+
   test "should get index" do
     get :index
     assert_response :success
   end
 
+  test "should get delete" do
+    get :delete
+    assert_response :success
+  end
+
+    test "should add a todo" do
+    assert_difference('Todo.count') do
+      post :add, todo: { todo_item: @todo_items.todo_item }
+    end
+
+    assert_redirected_to todo_path(assigns(:todo))
+  end
 end
--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Todo saves with all parameters" do
+    todo = Todo.create(todo_item: 'Todo item', completed: 'Todo compete')
+    assert todo.valid?, 'Todo did not save'
+    assert_equal 'Todo item', todo.todo_item, 'Post title does not match'
+  end
 end
